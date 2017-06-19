67b87fc63831a8c877741fa191924c0467af317c
jst <jst@jst.local>
2017-06-19 11:16:13 +0800

    Initial Commit

---
+++ audioMix_jst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  audioMix_jst
+//
+//  Created by jst on 2017/6/19.
+//  Copyright © 2017年 jst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ audioMix_jst/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  audioMix_jst
+//
+//  Created by jst on 2017/6/19.
+//  Copyright © 2017年 jst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
